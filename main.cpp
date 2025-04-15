54573df56604ebf4c86afaf28f66c93537147186
EqUiNoX <equinox.teamresurgent@gmail.com>
2025-04-15 15:55:54 -0600

    Remove const

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@
 
 #include "main.h"
 
-int RenderSVG(const uint8_t* svg_data, const uint8_t* image_data, uint16_t width, uint16_t height)
+int RenderSVG(const uint8_t* svg_data, uint8_t* image_data, uint16_t width, uint16_t height)
 {
     int pitch = width * 4;
 
--- main.h
+++ main.h
@@ -6,4 +6,4 @@
 #define TR_EXPORT extern "C" __attribute__((visibility("default")))
 #endif
 
-TR_EXPORT int RenderSVG(const uint8_t* svg_data, const uint8_t* image_data, uint16_t width, uint16_t height);
+TR_EXPORT int RenderSVG(const uint8_t* svg_data, uint8_t* image_data, uint16_t width, uint16_t height);
